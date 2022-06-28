5ee280e660ffbcde89507fa0fb8d3eabfb51d8ca
Rostislav Lipsky <ungaf@ya.ru>
2022-06-28 13:30:15 +0300

    Lil refactor

--- src/main/java/engineer/leepsky/ExampleApp.java
+++ src/main/java/engineer/leepsky/ExampleApp.java
@@ -33,13 +33,13 @@ public class ExampleApp {
             for (Token token : tokens) {
                 System.out.printf("%d:%02d\t | ", token.getLoc().row(), token.getLoc().col());
                 switch (token.toStringNL().substring(6, 8)) {
-                    case "Id" -> System.out.printf("IDENT\t | `%s`",    ((Token.Identifier)(token)).getName()       );
-                    case "Ke" -> System.out.printf("KEYWORD\t | `%s`",  ((Token.Keyword)(token)).getKind()          );
-                    case "Sp" -> System.out.printf("SPECIAL\t | `%s`",  ((Token.Special)(token)).getKind()          );
+                    case "Id" -> System.out.printf("IDENT\t | `%s`",    ((Token.Identifier)   (token)).getName()    );
+                    case "Ke" -> System.out.printf("KEYWORD\t | `%s`",  ((Token.Keyword)      (token)).getKind()    );
+                    case "Sp" -> System.out.printf("SPECIAL\t | `%s`",  ((Token.Special)      (token)).getKind()    );
                     case "St" -> System.out.printf("STRING\t | `%s`",   ((Token.StringLiteral)(token)).getContent() );
-                    case "In" -> System.out.printf("INT\t\t | `%s`",    ((Token.IntLiteral)(token)).getValue()      );
-                    case "Fl" -> System.out.printf("FLOAT\t | `%s`",    ((Token.FloatLiteral)(token)).getValue()    );
-                    case "Un" -> System.out.printf("Unparsed\t | `%s`", ((Token.Unparsed)(token)).getFail()         );
+                    case "In" -> System.out.printf("INT\t\t | `%s`",    ((Token.IntLiteral)   (token)).getValue()   );
+                    case "Fl" -> System.out.printf("FLOAT\t | `%s`",    ((Token.FloatLiteral) (token)).getValue()   );
+                    case "Un" -> System.out.printf("Unparsed\t | `%s`", ((Token.Unparsed)     (token)).getFail()    );
                     default -> System.out.println(token);
                 }
                 System.out.printf("%n");
